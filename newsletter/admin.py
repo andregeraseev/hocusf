44e608e943822bd603efb727ea6dec9f2d8deef9
Andre Geraseev <ageraseev@gmail.com>
2022-10-20 05:46:35 -0300

    mudando titulo send no newsletters

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -6,7 +6,7 @@ def send_newsletter(modeladmin, request, queryset):
 
     for newsletter in queryset:
         newsletter.send(request)
-        send_newsletter.short_description = "Enviar newsletter para todos os instritos"
+        send_newsletter.short_description = "Enviar newsletter"
 
 class NewsletterList(admin.ModelAdmin):
     list_display = ('id', 'corpo_show', 'mensagem_extra', 'created_at', 'updated_at')
